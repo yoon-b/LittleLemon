4d67abfacf5f86d161765024071c0e69f1f42283
Eunbi Kim <eunbi.kim.kor@gmail.com>
2024-01-18 12:15:40 +0900

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Eunbi Kim on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
